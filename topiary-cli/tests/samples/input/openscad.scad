6d864bfdee26f9bf5e90d3f90ac6c2de44ed1e71
Wayne Van Son <waynevanson@gmail.com>
2024-04-04 16:04:24 +1100

    feat: lists

--- topiary-cli/tests/samples/input/openscad.scad
+++ topiary-cli/tests/samples/input/openscad.scad
@@ -1,2 +1,30 @@
 
 function sup_bro   ()   =2  ;
+
+// comment
+
+/* another comment */
+
+/* multi
+ * line
+ * comment
+ */
+
+
+
+module name() {
+
+    translate([1, 2, 3])
+
+    translate([1, 2, 3])
+
+    translate([1, 2, 3])
+   
+    cube([1,2,3]);
+
+    translate([1, 2, 3])
+  
+    cube([1,2,3]);
+
+    cube();
+  }
--- topiary-queries/queries/openscad.scm
+++ topiary-queries/queries/openscad.scm
@@ -1,10 +1,117 @@
-(literal) @leaf
+;; Language specification has remarkable differences from the tree sitter grammar implemented.
+;; https://files.openscad.org/grammar.xhtml#module_instantiation
+;;
+;; One goal of this formatter is to give the OpenSCAD user full control
+;; of where to put their new lines, since topiary doesn't consider line length.
 
-; append_space
+;; Allow blank line before
 [
-  "function"
-  (parameters_declaration)
+  (function_declaration)
+  (module_declaration)
+  (function)
+  (comment)
+  (transform_chain)
+  (assignment)
+] @allow_blank_line_before
+
+;; semicolon means end of line
+";" @prepend_antispace @append_hardline
+
+;; Space before.
+[
+  (include_path)
+] @prepend_space
+
+;; Space around.
+[ 
   "="
+] @prepend_space @append_space
+
+
+;; Space after.
+[
+ "module"
+ ","
 ] @append_space
 
-";" @prepend_antispace @append_empty_softline
+(
+  function_declaration
+  "function" @append_space
+)
+
+;; Hardline after.
+[
+  (union_block)
+  (comment)
+] @append_hardline
+
+;; Hardline before.
+[
+  (use_statement)
+  (include_statement)
+] @prepend_hardline
+
+;; Function literals
+(
+  (
+    function
+    body: (_)
+      @prepend_input_softline
+      @prepend_indent_start
+      @append_indent_end
+  ) 
+)
+
+;; Union block with no children should be on one line
+;;
+;; module name() {}
+(
+  union_block
+  "{" @prepend_space @append_space
+  .
+  "}" @append_hardline
+)
+
+;; Union block with children should span it's content over
+;; multiple lines
+;;
+;; module name() {
+;;   child();
+;; }
+(
+  union_block
+  "{" @prepend_space @append_hardline @append_indent_start
+  (_)
+  "}" @prepend_indent_end @prepend_hardline
+)
+
+;; Lists
+
+;; Delete the comma if it's single line context
+(
+  list
+  (expression)
+  "," @delete
+  .
+  "]" @prepend_antispace
+  (#single_line_only!)
+) 
+
+;; Add comma if multi line context
+(
+  list
+  (expression) @append_delimiter
+  .
+  "]" @prepend_antispace
+  (#delimiter! ",")
+  (#multi_line_only!)
+) 
+
+;; format on multiple lines if 1 is multiline
+(
+  list
+  "[" @append_indent_start @append_hardline
+  ((_) "," @append_empty_softline)*
+  "]" @prepend_hardline @prepend_indent_end
+  (#multi_line_only!)
+)
